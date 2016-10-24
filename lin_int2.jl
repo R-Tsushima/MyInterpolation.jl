f7620cb4258b4e62400067e9a58fa1ff1ee78c3c
tsushima\Ryota Tsushima <r28.ynwa.06.dsmn.t@gmail.com>
2016-10-24 16:52:14 +0900

    A Simple Optimal Growth Model

---
+++ lin_int2.jl
@@ -0,0 +1,23 @@
+function lin_inter(grid,vals)
+    function func(x::Real)
+        if x < grid[1]
+            print("Error")
+        elseif x >= grid[end]
+            print("Error")
+        else
+            index = searchsortedlast(grid,x)
+            return (vals[index+1] - vals[index])/(grid[index+1] - grid[index])*(x - grid[index]) + vals[index]
+        end
+    end
+
+    function func{T<:Real}(x::AbstractVector{T})
+        m = length(x)
+        out = Array(Any,m)
+        for i in 1:m
+            out[i] = func(x[i])
+        end
+        return out
+    end
+    return func
+
+end
