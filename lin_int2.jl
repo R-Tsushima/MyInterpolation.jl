44f993d0aa308a7598c0520e2ad15cefe33dc7d7
tsushima\Ryota Tsushima <r28.ynwa.06.dsmn.t@gmail.com>
2016-10-31 20:44:08 +0900

    no message

--- lin_int2.jl
+++ lin_int2.jl
@@ -1,13 +1,9 @@
 function lin_inter(grid,vals)
     function func(x::Real)
-        if x < grid[1]
-            print("Error")
-        elseif x > grid[end]
-            print("Error")
-        elseif x == grid[1]
-            return vals[1]
-        elseif x == grid[end]
-            return vals[end]
+        if x <= grid[1]
+            return (vals[2] - vals[1])/(grid[2] - grid[1])*(x - grid[1]) + vals[1]
+        elseif x >= grid[end]
+            return (vals[end] - vals[end-1])/(grid[end] - grid[end-1])*(x - grid[end]) + vals[end]
         else
             index = searchsortedlast(grid,x)
             return (vals[index+1] - vals[index])/(grid[index+1] - grid[index])*(x - grid[index]) + vals[index]
