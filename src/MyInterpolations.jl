a08e6baaa20238c63821b34493a74dfa2ea4a2d4
LAPTOP-QT0PJ9P2\r28yn <r28.ynwa.06.dsmn.t@gmail.com>
2017-05-15 10:40:36 +0900

    関数修正

--- src/MyInterpolations.jl
+++ src/MyInterpolations.jl
@@ -1,14 +1,14 @@
 module MyInterpolations
 export my_lin_interp
 function my_lin_interp.(grid,vals)
-    function func.{T<:Real}(x::AbstractVector{T})
-        if .x <= grid[1]
-            return (vals[2] - vals[1])/(grid[2] - grid[1])*(.x - grid[1]) + vals[1]
-        elseif .x >= grid[end]
-            return (vals[end] - vals[end-1])/(grid[end] - grid[end-1])*(.x - grid[end]) + vals[end]
+    function func{T<:Real}(x::AbstractVector{T})
+        if x <= grid[1]
+            return (vals[2] - vals[1])/(grid[2] - grid[1])*(x - grid[1]) + vals[1]
+        elseif x >= grid[end]
+            return (vals[end] - vals[end-1])/(grid[end] - grid[end-1])*(x - grid[end]) + vals[end]
         else
-            index = searchsortedlast(grid,.x)
-            return (vals[index+1] - vals[index])/(grid[index+1] - grid[index])*(.x - grid[index]) + vals[index]
+            index = searchsortedlast(grid,x)
+            return (vals[index+1] - vals[index])/(grid[index+1] - grid[index])*(x - grid[index]) + vals[index]
         end
     end
 end
